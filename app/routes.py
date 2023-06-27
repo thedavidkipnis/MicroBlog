18ef7dd0c44c97beb59ddbedbd3d50a919cff951
David Kipnis <thedavidkipnis@gmail.com>
2023-06-26 21:37:53 -0700

    Added template support

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,26 @@
 # Micro Blog - Application
 
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello world!"
+def index() -> str:
+
+    user = {'username': 'Dave'}
+
+    posts = [
+        {
+            'author': {'username': 'Jim'},
+            'body': 'Wowza'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': "Jim's my bestest friend"
+        }
+    ]
+
+    body = render_template('index.html', title='Home', user=user, posts=posts)
+
+    return body
